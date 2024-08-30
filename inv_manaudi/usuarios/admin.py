83a3c1f9a6588f4120c909cc0bf3e2568a349664
Andres Sornoza <rolansor@hotmail.com>
2024-08-30 10:42:14 -0500

    Correciones menores.

--- inv_manaudi/usuarios/admin.py
+++ inv_manaudi/usuarios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+class UsuarioAdmin(UserAdmin):
+    # Aquí puedes personalizar el admin del modelo Usuario
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('telefono', 'direccion')}),
+    )
+
+admin.site.register(Usuario, UsuarioAdmin)
