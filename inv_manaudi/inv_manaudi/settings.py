b00a1ab7343a3ef3ae46d2fac0791733597ab6e7
Andres Sornoza <rolansor@hotmail.com>
2024-09-04 03:28:21 -0500

    Filtrado de Productos y registros.

--- inv_manaudi/inv_manaudi/settings.py
+++ inv_manaudi/inv_manaudi/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'inv_manaudi.wsgi.application'
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -70,7 +71,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
 '''
 DATABASES = {
     'default': {
@@ -82,7 +82,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-'''
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -112,9 +112,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 
