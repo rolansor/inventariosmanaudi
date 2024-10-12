7569e0eee1d47b0592c0489b3ac4f5a64348c95b
Andres Sornoza <rolansor@hotmail.com>
2024-10-12 03:00:16 -0500

    Restructuracion de inventarios.

--- inv_manaudi/categorias/urls.py
+++ inv_manaudi/categorias/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
 
     path('nueva_categoria/', views.nueva_categoria, name='nueva_categoria'),
     path('lista_categorias/', views.lista_categorias, name='lista_categorias'),
-    path('categorias/editar/<int:pk>/', views.editar_categoria, name='editar_categoria'),
-    path('categorias/eliminar/<int:pk>/', views.eliminar_categoria, name='eliminar_categoria'),
-    path('categorias/<int:pk>/nueva_subcategoria/', views.nueva_subcategoria, name='nueva_subcategoria'),
-    path('categorias/<int:pk>/lista_subcategorias/', views.lista_subcategorias, name='lista_subcategorias'),
-    path('categorias/subcategorias/editar/<int:pk>/', views.editar_subcategoria, name='editar_subcategoria'),
-    path('categorias/subcategorias/eliminar/<int:pk>/', views.eliminar_subcategoria, name='eliminar_subcategoria'),
+    path('editar/<int:pk>/', views.editar_categoria, name='editar_categoria'),
+    path('eliminar/<int:pk>/', views.eliminar_categoria, name='eliminar_categoria'),
+    path('<int:pk>/nueva_subcategoria/', views.nueva_subcategoria, name='nueva_subcategoria'),
+    path('<int:pk>/lista_subcategorias/', views.lista_subcategorias, name='lista_subcategorias'),
+    path('subcategorias/editar/<int:pk>/', views.editar_subcategoria, name='editar_subcategoria'),
+    path('subcategorias/eliminar/<int:pk>/', views.eliminar_subcategoria, name='eliminar_subcategoria'),
 ]
